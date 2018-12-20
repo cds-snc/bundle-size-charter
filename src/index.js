9538f60d3f33b6b0829fabae364158f622d24f01
Tim Arney <tim@line37.com>
2018-12-20 09:28:15 -0500

    temp datasets for chart

--- src/index.js
+++ src/index.js
@@ -4,22 +4,65 @@ const prettyBytes = require("pretty-bytes");
 
 const format = require("date-fns/format");
 
+const datasets = [];
+
+let colours = ["green", "yellow", "orange", "blue"];
+
+const setDataObj = (filename, data, index) => {
+  return {
+    label: filename,
+    borderColor: colours[index],
+    pointBorderColor: colours[index],
+    pointHoverBackgroundColor: colours[index],
+    pointHoverBorderColor: colours[index],
+    pointBackgroundColor: "#fff",
+    pointBorderWidth: 1,
+    pointHoverRadius: 5,
+    fill: false,
+    lineTension: 0.1,
+    data: data
+  };
+};
+
+/*
+
+*/
+
 const outputResult = result => {
+  let entries = [];
+
   result.forEach(entry => {
-    const { timestamp } = entry;
+    const { timestamp, sha } = entry;
 
     const ts = format(timestamp, "YYYY-MM-DD hh:mm:ss a");
-    console.log(ts);
+    entries.push(sha.substring(0, 6));
 
     entry.data.forEach(item => {
       item.files.forEach(file => {
-        const size = prettyBytes(file.filesize, { signed: true });
-        console.log(`${file.filename} ${size}`);
+        if (!datasets[file.filename]) {
+          datasets[file.filename] = {};
+          datasets[file.filename].data = [];
+        }
+
+        datasets[file.filename].data.push(file.filesize);
+        // console.log(`${file.filename} ${file.filesize}`);
       });
     });
 
-    console.log("===========================================");
+    //console.log("===========================================");
+    console.log(datasets);
   });
+
+  let chartDatasets = [];
+
+  Object.keys(datasets).map(function(key, index) {
+    const data = datasets[key].data;
+    chartDatasets.push(setDataObj(key, data, index));
+  });
+
+  //
+  console.log(chartDatasets);
+  //
 };
 
 module.exports.chartSize = async (request, response) => {
