d13a1f41bec4eb0a0d33c445e8176c61729e7236
Tim Arney <tim@line37.com>
2018-12-31 09:34:57 -0500

    render using pug direct

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 "use strict";
 import { loadFromFirestore } from "./lib/firestore";
 const { formatDataset } = require("./lib/formatDataset");
+const pug = require("pug");
+const path = require("path");
 
 module.exports.chartSize = async (request, response) => {
   if (!request.query.hasOwnProperty("repo")) {
@@ -14,10 +16,11 @@ module.exports.chartSize = async (request, response) => {
   const result = await loadFromFirestore(request.query.repo, branch);
 
   const dataset = formatDataset(result);
-
-  response.render("index", {
-    title: "Chart",
-    reponame: request.query.repo,
-    data: JSON.stringify(dataset)
-  });
+  response.send(
+    pug.renderFile(path.resolve(__dirname, "template.pug"), {
+      title: "Chart",
+      reponame: request.query.repo,
+      data: JSON.stringify(dataset)
+    })
+  );
 };
